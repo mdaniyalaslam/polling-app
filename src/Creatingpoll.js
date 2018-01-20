12855d64febc3fd7b2c9f48f9c9b409837efa393
M.Daniyal Aslam <mdaniyalaslam@gmail.com>
2018-01-20 14:16:54 +0500

    more

--- src/Creatingpoll.js
+++ src/Creatingpoll.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from "firebase";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Jumbotron, Button, Card } from 'react-bootstrap';
 
@@ -9,75 +10,117 @@ import {
 } from 'react-router-dom'
 
 class Creatingpoll extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            pollname : '',
+            question : '',
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        }
+    }
+
+    updateFormState(ev){
+
+        // let currentState = {}
+        // currentState[formLabel] = ev.target.value;
+        this.setState({[ev.target.name]:ev.target.value});
+        // console.log(currentState)
+
+    }
+    saveToFirebase() {
+        console.log(this.state, 'states');
+        let db = firebase.database();
+        let that = this;
+        db.ref('/polls/' + this.state.pollName + '/').once('value', function (snapshot) {
+            if (snapshot.val() === null) {
+                // console.log(this,'this',that.state.a, 'that');
+                that.setState({
+                    a: { option: that.state.a, vote: 0 },
+                    b: { option: that.state.b, vote: 0 },
+                    c: { option: that.state.c, vote: 0 },
+                    d: { option: that.state.d, vote: 0 }
+                })
+                // console.log(that.state);
+                let thatIs = that;
+                db.ref('/polls/' + that.state.pollName + '/').set(that.state)
+                    .then((success) => {
+                        console.log(success, 'success');
+                        thatIs.setState({
+                            pollName: '',
+                            question: '',
+                            a: '',
+                            b: '',
+                            c: '',
+                            d: ''
+                        })
+                    })
+                    .catch((error) => {
+                        console.log(error, 'error');
+                    })
+            }
+            else {
+                alert('This poll name is already exist, kindly change the poll name.');
+            }
+        })
+
+
+    }
+
     render() {
         return (
-
             <div>
-
                 <div className="container">
                     <form className="form-horizontal" role="form">
                         <fieldset>
                             <legend>Creating Poll</legend>
                             <div className="form-group">
-                                <label className="col-sm-3 control-label" for="card-holder-name">Name on Card</label>
-                                <div className="col-sm-9">
-                                    <input type="text" className="form-control" name="card-holder-name" id="card-holder-name" placeholder="Card Holder's Name" />
+                                <label className="col-sm-3 control-label" htmlFor="poll-name">Poll Name</label>
+                                <div className="col-sm-6">
+                                    <input type="text" name='pollname' className="form-control" value={this.state.pollname} onChange={this.updateFormState.bind(this)} id="pollname" placeholder="Write Poll Name" />
+                                </div> 
+                            </div>
+
+                            <div className="form-group">
+                                <label className="col-sm-3 control-label" htmlFor="poll">Question:</label>
+                                <div className="col-sm-6">
+                                    <textarea className="form-control" name='question' value={this.state.question} onChange={this.updateFormState.bind(this)} rows='3' placeholder='Write Poll Here...' />
                                 </div>
                             </div>
+
                             <div className="form-group">
-                                <label className="col-sm-3 control-label" for="card-number">Card Number</label>
-                                <div className="col-sm-9">
-                                    <input type="text" className="form-control" name="card-number" id="card-number" placeholder="Debit/Credit Card Number" />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label className="col-sm-3 control-label" for="expiry-month">Expiration Date</label>
+                                <label className="col-sm-3 control-label" htmlFor="">Options:</label>
                                 <div className="col-sm-9">
                                     <div className="row">
-                                        <div className="col-xs-3">
-                                            <select className="form-control col-sm-2" name="expiry-month" id="expiry-month">
-                                                <option>Month</option>
-                                                <option value="01">Jan (01)</option>
-                                                <option value="02">Feb (02)</option>
-                                                <option value="03">Mar (03)</option>
-                                                <option value="04">Apr (04)</option>
-                                                <option value="05">May (05)</option>
-                                                <option value="06">June (06)</option>
-                                                <option value="07">July (07)</option>
-                                                <option value="08">Aug (08)</option>
-                                                <option value="09">Sep (09)</option>
-                                                <option value="10">Oct (10)</option>
-                                                <option value="11">Nov (11)</option>
-                                                <option value="12">Dec (12)</option>
-                                            </select>
+                                        <div className="col-xs-4">
+                                            <input type="text" className="form-control" name="a" onChange={this.updateFormState.bind(this)} id="a" placeholder="Option-1" />
                                         </div>
-                                        <div className="col-xs-3">
-                                            <select className="form-control" name="expiry-year">
-                                                <option value="13">2013</option>
-                                                <option value="14">2014</option>
-                                                <option value="15">2015</option>
-                                                <option value="16">2016</option>
-                                                <option value="17">2017</option>
-                                                <option value="18">2018</option>
-                                                <option value="19">2019</option>
-                                                <option value="20">2020</option>
-                                                <option value="21">2021</option>
-                                                <option value="22">2022</option>
-                                                <option value="23">2023</option>
-                                            </select>
+                                        <div className="col-xs-4">
+                                            <input type="text" className="form-control" name="b" onChange={this.updateFormState.bind(this)} id="b" placeholder="Option-2" />
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                             <div className="form-group">
-                                <label className="col-sm-3 control-label" for="cvv">Card CVV</label>
-                                <div className="col-sm-3">
-                                    <input type="text" className="form-control" name="cvv" id="cvv" placeholder="Security Code" />
+                                <label className="col-sm-3 control-label" htmlFor="expiry-month"></label>
+                                <div className="col-sm-9">
+                                    <div className="row">
+                                        <div className="col-xs-4">
+                                            <input type="text" className="form-control" name="c" onChange={this.updateFormState.bind(this)} id="c" placeholder="Option-3" />
+                                        </div>
+                                        <div className="col-xs-4">
+                                            <input type="text" className="form-control" name="d" onChange={this.updateFormState.bind(this)} id="d" placeholder="Option-4" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                             <div className="form-group">
                                 <div className="col-sm-offset-3 col-sm-9">
-                                    <button type="button" className="btn btn-success">Pay Now</button>
+                                    <button type="submit" onClick={this.saveToFirebase.bind(this)} className="btn btn-success">Create</button>
                                 </div>
                             </div>
                         </fieldset>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import *as firebase from "firebase";
+
 
 import './index.css';
 // import App from './App';
@@ -11,7 +14,17 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import MyRoutes from './Routes';
 
-import registerServiceWorker from './registerServiceWorker';
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDQbQtFRvu5-W41ht-SdXHCph5sD5-sqY",
+    authDomain: "fir-4c8f9.firebaseapp.com",
+    databaseURL: "https://fir-4c8f9.firebaseio.com",
+    projectId: "fir-4c8f9",
+    storageBucket: "fir-4c8f9.appspot.com",
+    messagingSenderId: "1092056266627"
+  };
+  firebase.initializeApp(config);
+
 
 ReactDOM.render(<MyRoutes />, document.getElementById('root'));
 registerServiceWorker();
